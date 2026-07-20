4ad34905155e8df4bef982f95b501cd7740d88b4
drocha05 <dariorocha710@gmail.com>
2026-07-20 19:21:03 -0300

    punto 3

--- src/tp.pl
+++ src/tp.pl
@@ -16,6 +16,10 @@ habitante(lernen, auberst, 1315, humano).
 habitante(frieren, weise, 100, elfo).
 habitante(eisen, riegel, 1150, enano).
 
+habitante(humano_default, pueblo, 2000, humano).
+habitante(enano_default, pueblo, 2000, enano).
+habitante(elfo_default, pueblo, 2000, elfo).
+
 % Parte b
 
 promedio_vida(humano,80).
@@ -86,18 +90,22 @@ recuerda(Persona, NombreHazania, Anio) :-
     conmemorar_hazanias(NombreHazania , dia_festivo(AnioFestival), Pueblo),
     Anio >= AnioFestival.
 
-buen_estado(marmol, AnioConstruccion, _ , Anio) :-
+buen_estado(marmol, AnioConstruccion, _, Anio) :-
+    Anio >= AnioConstruccion,
     Anio - AnioConstruccion =< 30.
 
 buen_estado(marmol, _ , ListaMantenimiento, Anio) :-
     member(AnioMantenimiento, ListaMantenimiento),
+    Anio >= AnioMantenimiento,
     Anio - AnioMantenimiento =< 30.
 
-buen_estado(bronce, AnioConstruccion, _ , Anio) :-
+buen_estado(bronce, AnioConstruccion, _, Anio) :-
+    Anio >= AnioConstruccion,
     Anio - AnioConstruccion =< 15.
 
 buen_estado(bronce, _ , ListaMantenimiento, Anio) :-
     member(AnioMantenimiento, ListaMantenimiento),
+    Anio >= AnioMantenimiento,
     Anio - AnioMantenimiento =< 15.
 
 
@@ -158,32 +166,32 @@ test("Una persona enana esta viva en promedio hasta 350 años despues de su naci
 test("Una persona elfo no esta vivo antes de su nacimiento"):- 
     not(estaViva(elfo_default,1900)).
     
-test("Si una persona presencia una hazaña la recuerda por el resto de su vida desde ese momento"):-
+test("Si una persona presencia una hazaña la recuerda por el resto de su vida desde ese momento", nondet):-
     recuerda(wirbel, rescatar_hermana_de_wirbel, 1400),
     not(recuerda(wirbel, rescatar_hermana_de_wirbel, 1300)),
     not(recuerda(wirbel, rescatar_hermana_de_wirbel, 1500)).
-test("Si una persona escucho una canción sobre esa hazaña la recuerda por 15 años desde ese momento"):-
+test("Si una persona escucho una canción sobre esa hazaña la recuerda por 15 años desde ese momento", nondet):-
     recuerda(lawine, destruir_demonio_aura, 1400),
     not(recuerda(lawine, destruir_demonio_aura, 1380)),
     not(recuerda(lawine, destruir_demonio_aura, 1410)).
-test("Si una persona leyó un libro sobre una hazaña, la recuerda por tantos años como paginas que tenga el libro"):-
+test("Si una persona leyó un libro sobre una hazaña, la recuerda por tantos años como paginas que tenga el libro", nondet):-
     recuerda(voll, destruir_demonio_aura, 1450),
     not(recuerda(voll, destruir_demonio_aura, 1460)),
     not(recuerda(voll, destruir_demonio_aura, 1300)).
 test("Si en el pueblo en el que vive una persona se conmemora una hazaña con un dia festivo la recuerda por el resto de su vida desde ese momento"):-
     recuerda(fern, destruir_rey_demonio, 1400),
-    not(recuerda(denken, destruir_rey_demonio, 1400)).
+    not(recuerda(voll, destruir_rey_demonio, 1400)).
 
-test("Si en el pueblo en el que vive una persona hay una estatua que conmemora una hazaña, la recuerda si sigue estando en buen estado"):-
+test("Si en el pueblo en el que vive una persona hay una estatua que conmemora una hazaña, la recuerda si sigue estando en buen estado", nondet):-
     recuerda(lawine, destruir_rey_demonio, 1400),
     not(recuerda(lawine, destruir_rey_demonio, 1390)).
 
-test("Una hazaña esta corroborada si solo hay una versión de la misma"):-
+test("Una hazaña esta corroborada si solo hay una versión de la misma", nondet):-
     corroborada(rescatar_hermana_de_wirbel).
-test("Una hazaña no esta corrobarada si hubo diferentes personas que la llevaron a cabo o diferente lugar en el que ocurrió la hazaña"):-
+test("Una hazaña no esta corrobarada si hubo diferentes personas que la llevaron a cabo o diferente lugar en el que ocurrió la hazaña", nondet):-
     not(corroborada(destruir_demonio_aura)).
 
-test("Una hazaña pasa al olvido si ya nadie la recuerda ese año"):-
+test("Una hazaña pasa al olvido si ya nadie la recuerda ese año", nondet):-
     al_olvido(destruir_demonio_aura, 2000),
     not(al_olvido(rescatar_hermana_de_wirbel, 1400)).
 
