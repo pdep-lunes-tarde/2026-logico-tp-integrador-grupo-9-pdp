bf763774690266678b697880a044bd6a3d7da337
drocha05 <dariorocha710@gmail.com>
2026-08-24 11:07:51 -0300

    Correccion dreamTeam

--- src/tp.pl
+++ src/tp.pl
@@ -220,9 +220,23 @@ cadenaValida(Heroe, HeroesEnLista, [Heroe, HeroeInfluenciado | Resto]) :-
 
 % Punto 6
 
-cadenaDeAntecesores(Heroe, Antecesores):-
-    cadenaDeInspiracion(_, Antecesores),
-    last(Antecesores, Heroe).
+esAntecesorDe(Antecesor, Heroe) :-
+    cadenaDeInspiracion(Antecesor, Cadena),
+    last(Cadena, Heroe),
+    Antecesor \= Heroe.
+
+antecesoresElegidos(Heroe, HeroesEnLista, [Antecesor]) :-
+    esAntecesorDe(Antecesor, Heroe),
+    not(member(Antecesor, HeroesEnLista)).
+
+antecesoresElegidos(Heroe, HeroesEnLista, [Antecesor | Resto]) :-
+    esAntecesorDe(Antecesor, Heroe),
+    not(member(Antecesor, HeroesEnLista)),
+    antecesoresElegidos(Heroe, [Antecesor | HeroesEnLista], Resto).
+
+dreamTeam(Heroe, Equipo) :-
+    antecesoresElegidos(Heroe, [Heroe], Antecesores),
+    permutation([Heroe | Antecesores], Equipo).
 
 :- begin_tests(tpIntegrador, []).
 
