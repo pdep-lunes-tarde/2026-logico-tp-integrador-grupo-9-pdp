44197c583b4fe39c3ad147e1d821a3eba4bc73d8
drocha05 <dariorocha710@gmail.com>
2026-08-24 00:13:13 -0300

    Tests comentados

--- src/tp.pl
+++ src/tp.pl
@@ -178,17 +178,8 @@ puebloMusical(Anio, Pueblo):-
             HazaniasRecordadasPorOtrosMedios),
     length(HazaniasRecordadasPorCancion, CantidadDeHazaniasRecordadasPorCancion),
     length(HazaniasRecordadasPorOtrosMedios, CantidadDeHazaniasRecordadasPorOtrosMedios),
-    CantidadDeHazaniasRecordadasPorCancion >= CantidadDeHazaniasRecordadasPorOtrosMedios.
-/*
-puebloMusical(Anio, Pueblo):-
-    esPueblo(Pueblo),
-    findall(NombreHazania, (puebloRecuerdaHazania(Anio, NombreHazania, Pueblo), conocimiento(_, hazania(NombreHazania, _, _), _, escucho_cancion)), HazaniasRecordadasPorCancion),
-    findall(NombreHazania, (puebloRecuerdaHazania(Anio, NombreHazania, Pueblo), conocimiento(_, hazania(NombreHazania, _, _), _, _)), HazaniasRecordadasConRepeticion),
-    length(HazaniasRecordadasPorCancion, CantidadDeHazaniasRecordadasPorCancion),
-    sort(HazaniasRecordadasConRepeticion, HazaniasRecordadas),
-    length(HazaniasRecordadas, CantidadDeHazaniasRecordadas),
-    CantidadDeHazaniasRecordadasPorCancion >= CantidadDeHazaniasRecordadas / 2 .
-*/
+    CantidadDeHazaniasRecordadasPorCancion > CantidadDeHazaniasRecordadasPorOtrosMedios.
+
 puebloChismoso(Anio, Pueblo):-
     esPueblo(Pueblo),
     forall(puebloRecuerdaHazania(Anio, NombreHazania, Pueblo), not(corroborada(NombreHazania))).
@@ -218,21 +209,35 @@ inspiroAHeroe(Heroe, HeroeInspirador):-
 cadenaDeInspiracion(Heroe, Cadena) :-
     cadenaValida(Heroe, [Heroe], Cadena).
 
-cadenaValida(Heroe, Visitados, [Heroe, HeroeInfluenciado]):-
+cadenaValida(Heroe, HeroesEnLista, [Heroe, HeroeInfluenciado]):-
     inspiroAHeroe(HeroeInfluenciado, Heroe),
-    not(member(HeroeInfluenciado, Visitados)).
+    not(member(HeroeInfluenciado, HeroesEnLista)).
 
-cadenaValida(Heroe, Visitados, [Heroe, HeroeInfluenciado | Resto]) :-
+cadenaValida(Heroe, HeroesEnLista, [Heroe, HeroeInfluenciado | Resto]) :-
     inspiroAHeroe(HeroeInfluenciado, Heroe),
-    not(member(HeroeInfluenciado, Visitados)),
-    append(Visitados, [HeroeInfluenciado], VisitadosActualizado),
-    cadenaValida(HeroeInfluenciado, VisitadosActualizado, [HeroeInfluenciado | Resto]).
+    not(member(HeroeInfluenciado, HeroesEnLista)),
+    append(HeroesEnLista, [HeroeInfluenciado], HeroesEnListaActualizado),
+    cadenaValida(HeroeInfluenciado, HeroesEnListaActualizado, [HeroeInfluenciado | Resto]).
 
 % Punto 6
 
-cadenaDeAntecesores(Heroe, Antecesores):-
-    cadenaDeInspiracion(_, Antecesores),
-    last(Antecesores, Heroe).
+esAntecesorDe(Antecesor, Heroe) :-
+    cadenaDeInspiracion(Antecesor, Cadena),
+    last(Cadena, Heroe),
+    Antecesor \= Heroe.
+
+antecesoresElegidos(Heroe, HeroesEnLista, [Antecesor]) :-
+    esAntecesorDe(Antecesor, Heroe),
+    not(member(Antecesor, HeroesEnLista)).
+
+antecesoresElegidos(Heroe, HeroesEnLista, [Antecesor | Resto]) :-
+    esAntecesorDe(Antecesor, Heroe),
+    not(member(Antecesor, HeroesEnLista)),
+    antecesoresElegidos(Heroe, [Antecesor | HeroesEnLista], Resto).
+
+dreamTeam(Heroe, Equipo) :-
+    antecesoresElegidos(Heroe, [Heroe], Antecesores),
+    permutation([Heroe | Antecesores], Equipo).
 
 :- begin_tests(tpIntegrador, []).
 
@@ -273,6 +278,16 @@ test("Ninguna persona recuerda una hazaña antes de conocerla", nondet) :-
     not(recuerda(lawine, destruir_demonio_aura, 1380)), % Lawine conocio esta hazaña en 1393
     not(recuerda(voll, destruir_demonio_aura, 1300)). % Voll conocio esta hazaña en 1400
 
+test("Ninguna persona recuerda una hazaña antes de su año de nacimiento", nondet):-
+    not(recuerda(wirbel, rescatar_hermana_de_wirbel, 1300)), % Wirbel nacio en 1350 
+    not(recuerda(lawine, destruir_demonio_aura, 1300)), % Lawine nacio en 1372
+    not(recuerda(voll, destruir_demonio_aura, 1100)). % Voll nacio en 1200
+
+test("Ninguna persona recuerda una hazaña si ya falleció", nondet):-
+    not(recuerda(wirbel, rescatar_hermana_de_wirbel, 2000)), % Wirbel es un humano nacido en 1350 , ya estaría muerto pasados 80 años
+    not(recuerda(lawine, destruir_demonio_aura, 2000)), % Lawine es un humano nacido en 1372, ya estaría muerto pasados 80 años
+    not(recuerda(voll, destruir_demonio_aura, 2000)). % Voll es un enano nacido en 1200, ya estaría muerto pasados 350 años
+
 test("Si en el pueblo en el que vive una persona se conmemora una hazaña con un dia festivo la recuerda por el resto de su vida desde ese momento", nondet):-
     recuerda(fern, destruir_rey_demonio, 1400), % Fern vive en Weise, donde hay un dia festivo para la hazaña destruir_rey_demonio desde 1340
     not(recuerda(voll, destruir_rey_demonio, 1400)). % Voll vive en Ende, donde no se conmemora la hazaña destruir_rey_demonio
@@ -282,7 +297,7 @@ test("Si en el pueblo en el que vive una persona hay una estatua que conmemora u
     not(recuerda(lawine, destruir_rey_demonio, 1390)). % En 1390 la estatua de bronce de destruir_rey_demonio no se encuentra en buen estado
 
 test("Una persona que ya falleció NO recuerda una hazaña conmemorada en su pueblo", nondet) :-
-    not(recuerda(lernen, destruir_rey_demonio, 1410)). % En1410 la estatua esta en buen estado, pero Lernen ya esta muerto.
+    not(recuerda(lernen, destruir_rey_demonio, 1410)). % En 1410 la estatua esta en buen estado, pero Lernen ya esta muerto.
 
 test("Una persona NO recuerda una hazaña conmemorada antes de su año de nacimiento", nondet) :-
     not(recuerda(lawine, destruir_schlat_el_omnisciente, 1360)). % En 1360 la estatua estaba en buen estado, pero Lawine no habia nacido.
@@ -295,34 +310,57 @@ test("Una hazaña NO esta corrobarada si hubo diferentes personas que la llevaro
 
 test("Una hazaña pasa al olvido si ya nadie la recuerda ese año", nondet):-
     al_olvido(destruir_demonio_aura, 2000), % Para el año 2000 quienes tenian conocimiento de la hazaña destruir_demonio_aura (Voll y Lawine) ya no la recordarian
-    not(al_olvido(rescatar_hermana_de_wirbel, 1400)). % Para el año 1400 Wirbel y Freiren recordarian la hazaña rescatar_hermana_de_wirbel
+    not(al_olvido(rescatar_hermana_de_wirbel, 1400)). % Para el año 1400 Wirbel y Freiren recordarian la hazaña rescatar_hermana_de_wirbel.
 
-test("Una hazaña es recordada por un pueblo si todos sus habitantes la recuerdan", nondet):-
-    puebloRecuerdaHazania(1400, destruir_rey_demonio, weise),
-    puebloRecuerdaHazania(1395, rescatar_hermana_de_wirbel, klares),
-    not(puebloRecuerdaHazania(1395, destruir_rey_demonio, klares)).
+test("Una hazaña es recordada por un pueblo en cierto año si al menos una persona que vive en ese pueblo recuerda esa hazaña en ese año", nondet):-
+    puebloRecuerdaHazania(1400, destruir_rey_demonio, weise), % En Weise viven Serie, Fern, Kanne y Frieren, donde se conmemora la hazaña destruir_rey_demonio desde 1340, en 1400 todos recuerdan dicha hazaña
+    puebloRecuerdaHazania(1500, destruir_rey_demonio, weise), % Para el año 1500 en Weise, Fern y Kanne ya habrian muerto de vejez, pero Serie y Frieren al ser elfos todavia recordarian dicha hazaña
+    puebloRecuerdaHazania(1395, rescatar_hermana_de_wirbel, klares), % En Klares vive Wirbel, quien presencio la hazaña rescaratar_hermana_de_wirbel en 1390 y en 1395 recuerda dicha hazaña
+    not(puebloRecuerdaHazania(1395, destruir_rey_demonio, klares)), % En Klares no vive nadie que recuerda la hazaña destruir_rey_demonio, ya que nunca se tuvo conocimiento de dicha hazaña en ese pueblo
+    not(puebloRecuerdaHazania(1500, rescatar_hermana_de_wirbel, klares)). % En Klares en el año 1500 Wirbel ya habria muerto, por lo que nadie recordaria la hazaña rescatar_hermana_de_wirbel en el pueblo
 
 test("El total de paginas leidas por un pueblo es la sumatoria de las cantidades leidas por cada habitante", nondet):-
-    paginasLeidas(weise, 1335, 100),
-    paginasLeidas(weise, 1336, 0).
+    paginasLeidas(weise, 1335, 100), % En Weise, en 1335, Serie leyo 100 paginas
+    paginasLeidas(weise, 1336, 0). % En Weise, en 1336, ningun habitante leyo paginas
 
 test("El pueblo mas lector es aquel que hasta ese año sus habitantes son los que mas leyeron", nondet):-
-    puebloMasLector(1400, ende).
+    puebloMasLector(1400, ende). % En Ende, en 1400, Voll leyo 50 paginas, mientras que en los otros pueblos se leyeron 0 paginas
 
 test("Cuando la mayoria de hazañas recordadas en un pueblo son recordadas mediante canciones entonces es un pueblo musical", nondet):-
-    puebloMusical(1395, auberst),
-    not(puebloMusical(1400, weise)).
+    puebloMusical(1395, auberst), % En Auberst, en 1395, solo recuerdan la hazaña destuir_demonio_aura porque Lawine escucho una canción en 1393, las estatuas que conmemoran otras hazañas estan en mal estado
+    not(puebloMusical(1410, auberst)), % En Auberst, en 1410, las estatuas estan en buen estado y Lawine ya no recuerda la hazaña destruir_demonio_aura
+    not(puebloMusical(1400, weise)). % En Weise no hay hazañas recordadas mediante canciones
 
 test("Un pueblo es chismoso si todas las hazañas que se recuerdan no estan corroboradas", nondet):-
-    puebloChismoso(1420, ende),
-    not(puebloChismoso(1400, weise)).
+    puebloChismoso(1420, ende), % En Ende, en 1420, solo recuerdan la hazaña destruir_demonio_aura, que no esta corroborada
+    not(puebloChismoso(1400, weise)). % En Weise, en 1400, no todas las que se recuerdan no estan corroboradas
 
 test("Si todos los habitantes que viven en un pueblo en cierto año recuerdan una hazaña entonces la misma es importante", nondet):-
-    hazaniaImportante(1400, destruir_rey_demonio, weise),
-    not(hazaniaImportante(1400, recuperar_gato_perdido, weise)).
+    hazaniaImportante(1400, destruir_rey_demonio, weise), % En Weise, en 1400, todos los habitantes recuerdan la hazaña destruir_rey_demonio por el dia festivo
+    not(hazaniaImportante(1400, recuperar_gato_perdido, weise)). % En Weise, en 1400, solo kanne recuerda la hazaña recuperar_gato_perdido porque la presencio
 
-test("Un pueblo vive tiempos sin precedentes si todas las hazañas importantes del pueblo fueron precenciadas por alguien", nondet):-
-    tiemposSinPrecedentes(1394, klares),
-    not(tiemposSinPrecedentes(1400, weise)).
+test("Un pueblo vive tiempos sin precedentes si todas las hazañas importantes del pueblo fueron presenciadas por alguien", nondet):-
+    tiemposSinPrecedentes(1394, klares), % En Klares, en 1394, Wirbel presencio todas las hazañas importantes (rescatar_hermana_de_wirbel)
+    not(tiemposSinPrecedentes(1400, weise)). % En weise, en 1400, la hazaña destuir_rey_demonio es importante pero nadie del pueblo la presencio
+
+test("Cualquiera que haya participado en una hazaña conocida es un heroe", nondet):-
+    esHeroe(fern), % Fern participo en la hazaña rescatar_hermana_de_wirbel
+    not(esHeroe(wirbel)). % Wirbel no participo en ninguna hazaña
+
+test("Un heroe es inspirado por otro heroe si este ultimo participo en las hazañas que el primer heroe conocio", nondet):-
+    inspiroAHeroe(fern, frieren), % Fern conoce destruir_rey_demonio donde Frieren participo
+    inspiroAHeroe(frieren, fern), % Frieren conoce rescatar_herman_de_wirbel donde Fern participo
+    not(inspiroAHeroe(eisen, _)). % Nadie inspiro a Eisen, ya que no ssabemos ninguna hazaña que conozca
+
+test("Una cadena de inspiracion parte de un heroe inicial siguiendo todos los diferentes caminos por los que influencio a otros heroes", nondet):-
+    cadenaDeInspiracion(himmel, [himmel, fern, frieren, denken]), % Himmel inspiro a Fern, Fern inspiro a Frieren y Frieren inspiro a Denken
+    not(cadenaDeInspiracion(denken, [denken, frieren])), % Denken no inspiro a Frieren
+    not(cadenaDeInspiracion(himmel, [himmel, frieren, fern, frieren])). % Si bien Frieren inspiro a Fern y Fern inspiro a Frieren, no se pueden repetir heroes en la cadena
+
+test("Un Dream Team para un heroe es un equipo que incluya al heroe y tambien incluya antecesores", nondet):-
+    dreamTeam(fern, [fern, himmel]), % Es un equipo valido ya que Himmel es antecesor de Fern
+    dreamTeam(fern, [himmel, fern]), % Es un equipo valido, no importa el orden de los miembros en el equipo
+    not(dreamTeam(fern, [fern])), % No es un equipo valido ya que no incluye antecesoress
+    not(dreamTeam(fern, [frieren, himmel])). % No es un equipo valido ya que no incluye a Fern
 
 :- end_tests(tpIntegrador).
